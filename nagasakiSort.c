c25f9c37c2f05ac688ecd2bb87a399f50b11c00f
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2024-10-29 21:14:53 +0000

    Create nagasakiSort.c

---
+++ nagasakiSort.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int* nagasakiSort(int* fallout, int fallout_size, int* survivors) {
+    if (fallout_size <= 1) {
+        *survivors = fallout_size;
+        return fallout;
+    }
+
+    int blast_radius = (int)sqrt(fallout_size);
+    int* safe_zone = (int*)malloc(blast_radius * sizeof(int));
+    int population = 0;
+
+    safe_zone[population++] = fallout[0];
+
+    for (int i = 1; i < fallout_size && population < blast_radius; i++) {
+        if (fallout[i] >= safe_zone[population - 1]) {
+            safe_zone[population++] = fallout[i];
+        }
+    }
+
+    for (int i = 1; i < population; i++) {
+        if (safe_zone[i] < safe_zone[i - 1]) {
+            free(safe_zone);
+            *survivors = 0;
+            return NULL;
+        }
+    }
+
+    *survivors = population;
+    return safe_zone;
+}
+
+int main() {
+    int fallout[] = {1, 5, 2, 3, 8, 6, 7, 10, 12, 11, 88, 45, 35, 68, 9, 687, 5, 23, 56, 78, 43, 90, 15, 27, 19, 50, 73, 81, 96, 102, 67, 38, 29, 59, 123, 134, 144, 199, 210, 305, 407, 506, 608, 701, 85, 95, 107, 207, 307, 408, 510, 612, 750, 888, 920, 1001, 1115, 1212, 1325, 1423, 1538, 1647, 1777, 1899, 1934, 2000};
+    
+    int fallout_size = sizeof(fallout) / sizeof(fallout[0]);
+    int survivors;
+
+    int* safe_zone = nagasakiSort(fallout, fallout_size, &survivors);
+
+    if (safe_zone == NULL) {
+        printf("After the blast... unfortunately, no survivors detected. Everything is gone!\n");
+    } else {
+        printf("Survivors from the blast (Nagasaki Sort Result):\n");
+        for (int i = 0; i < survivors; i++) {
+            printf("%d ", safe_zone[i]);
+        }
+        printf("\nEvacuation successful! Number of survivors: %d\n", survivors);
+        free(safe_zone);
+    }
+
+    return 0;
+}
